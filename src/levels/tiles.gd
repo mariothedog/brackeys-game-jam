e1be5d6c2b91fdc2bec7b69d68c9222c6b09e950
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-02-26 23:25:45 +0000

    Add enemy path tiles to the main tileset

--- src/levels/tiles.gd
+++ src/levels/tiles.gd
@@ -6,14 +6,21 @@ const MAIN_TILE_SET = preload("res://levels/tileset.tres")
 var LevelEditor := {
 	"GROUND": LEVEL_EDITOR_TILE_SET.find_tile_by_name("ground"),
 	"WALL": LEVEL_EDITOR_TILE_SET.find_tile_by_name("wall"),
+	"ENEMY_PATH": LEVEL_EDITOR_TILE_SET.find_tile_by_name("enemy_path"),
+	"ENEMY_PATH_START": LEVEL_EDITOR_TILE_SET.find_tile_by_name("enemy_path_start"),
+	"ENEMY_PATH_END": LEVEL_EDITOR_TILE_SET.find_tile_by_name("enemy_path_end"),
 }
 
 var Main := {
 	"GROUND": MAIN_TILE_SET.find_tile_by_name("ground"),
 	"WALL": MAIN_TILE_SET.find_tile_by_name("wall"),
+	"ENEMY_PATH": MAIN_TILE_SET.find_tile_by_name("enemy_path")
 }
 
 var level_editor_to_main := {
 	LevelEditor.GROUND: Main.GROUND,
 	LevelEditor.WALL: Main.WALL,
+	LevelEditor.ENEMY_PATH: Main.ENEMY_PATH,
+	LevelEditor.ENEMY_PATH_START: Main.ENEMY_PATH,
+	LevelEditor.ENEMY_PATH_END: Main.ENEMY_PATH,
 }

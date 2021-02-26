6106255ab9134e4fdcd6b1467ae677fe4193d568
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-02-26 23:22:47 +0000

    Add a warning if there is no corresponding main tile for a level editor tile

--- src/main/level.gd
+++ src/main/level.gd
@@ -5,4 +5,7 @@ func build_level(level_data: LevelData) -> void:
 	for type in level_data.tiles:
 		var tiles: PoolVector2Array = level_data.tiles[type]
 		for pos in tiles:
-			set_cellv(pos, Tiles.level_editor_to_main[type])
+			if Tiles.level_editor_to_main.has(type):
+				set_cellv(pos, Tiles.level_editor_to_main[type])
+			else:
+				push_warning("Level editor tile %s has no corresponding main tile" % type)

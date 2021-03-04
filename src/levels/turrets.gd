b928e884b435a3e99ecc599989304ae649f6ee90
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-03-04 22:02:26 +0000

    Fix turret render order issues

--- src/levels/turrets.gd
+++ src/levels/turrets.gd
@@ -27,7 +27,14 @@ func _process(_delta: float) -> void:
 	_selected_turret.position = get_local_mouse_position()
 
 
+func _select_turret(turret: Turret) -> void:
+	turret.z_index = 2
+	_selected_turret = turret
+	set_process(true)
+
+
 func _release_turret(turret: Turret) -> void:
+	turret.z_index = 0
 	set_process(false)
 	_selected_turret = null
 
@@ -49,12 +56,22 @@ func _snap_turret_to_tile(turret: Turret, tile_pos: Vector2) -> void:
 	turret.global_position = world_pos_centered
 
 
+func _is_top_overlapping_turret(turret: Turret) -> bool:
+	var pos_in_parent := turret.get_position_in_parent()
+	for child in get_children():
+		if (
+			child.position == turret.position
+			and child != turret
+			and pos_in_parent < child.get_position_in_parent()
+		):
+			return false
+	return true
+
+
 func _on_item_button_down(_item: Item) -> void:
 	var turret: Turret = TURRET_SCENE.instance()
-	_selected_turret = turret
-	_selected_turret.position = get_local_mouse_position()
 	add_child(turret)
-	set_process(true)
+	_select_turret(turret)
 
 
 func _on_item_button_up(_item: Item) -> void:
@@ -62,7 +79,9 @@ func _on_item_button_up(_item: Item) -> void:
 
 
 func _on_draggable_turret_button_down(turret: Turret) -> void:
-	_selected_turret = turret
+	if not _is_top_overlapping_turret(turret):
+		return
+	_select_turret(turret)
 	set_process(true)
 
 

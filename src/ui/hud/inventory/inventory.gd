c5e57b45331feed4e29ba94a17bb10cbc755edaf
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-03-06 00:08:23 +0000

    Fix inventory slide in/out jitter bug

--- src/ui/hud/inventory/inventory.gd
+++ src/ui/hud/inventory/inventory.gd
@@ -10,6 +10,8 @@ func _process(_delta: float) -> void:
 	# The mouse_entered/mouse_exited signals are not enough
 	# Related issue: https://github.com/godotengine/godot/issues/16854
 	var is_mouse_inside := _is_mouse_inside()
+	if not _is_mouse_inside_viewport():
+		return
 	if is_mouse_inside and not _prev_is_mouse_inside:
 		emit_signal("mouse_entered_background")
 	elif not is_mouse_inside and _prev_is_mouse_inside:
@@ -19,3 +21,7 @@ func _process(_delta: float) -> void:
 
 func _is_mouse_inside() -> bool:
 	return get_global_rect().has_point(get_global_mouse_position())
+
+
+func _is_mouse_inside_viewport() -> bool:
+	return get_viewport_rect().has_point(get_global_mouse_position())

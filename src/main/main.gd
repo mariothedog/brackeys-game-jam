a296a4a95cc10b3a6e05d377d052e41b191bdf00
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-03-11 00:40:41 +0000

    Queue free bullets when stop is pressed

--- src/main/main.gd
+++ src/main/main.gd
@@ -5,6 +5,7 @@ export var dict := {}
 
 onready var step_delay: Timer = $StepDelay
 onready var enemies: Node2D = $Level/Enemies
+onready var bullets: Node2D = $Turrets/Bullets
 onready var placed_turrets: Node2D = $Turrets/PlacedTurrets
 
 
@@ -26,5 +27,6 @@ func _start() -> void:
 func _stop() -> void:
 	step_delay.stop()
 	Util.queue_free_children(enemies)
+	Util.queue_free_children(bullets)
 	for turret in placed_turrets.get_children():
 		turret.enable_sight_lines()

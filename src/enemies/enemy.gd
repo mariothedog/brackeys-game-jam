9fb2c2adc6b49aa19db5a7fc4938490542204798
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-03-22 19:21:53 +0000

    Make enemy and bullet colliders move smoothly as well

--- src/enemies/enemy.gd
+++ src/enemies/enemy.gd
@@ -14,21 +14,18 @@ var _path_length: int
 var _path_current_index := 0
 var _target_pos: Vector2
 
-onready var sprite: Sprite = $Sprite
-
-
 func _ready() -> void:
 	set_physics_process(false)
 
 
 func _physics_process(delta: float) -> void:
-	if sprite.global_position.is_equal_approx(_target_pos):
+	if global_position.is_equal_approx(_target_pos):
 		set_physics_process(false)
-		sprite.position = Vector2.ZERO
+		global_position = _target_pos
 		if _path_current_index + 1 == _path_length:
 			emit_signal("reached_end_of_path")
 		return
-	sprite.global_position = sprite.global_position.linear_interpolate(
+	global_position = global_position.linear_interpolate(
 		_target_pos, MOVEMENT_RATE * delta
 	)
 
@@ -38,9 +35,6 @@ func update_position_along_path() -> void:
 		return
 	_path_current_index += 1
 	_target_pos = path[_path_current_index]
-	var prev_global_pos := global_position
-	global_position = _target_pos
-	sprite.global_position = prev_global_pos
 	set_physics_process(true)
 
 

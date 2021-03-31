ae6a422a5f6ff5625b447e49f0b26f7c2876bc7e
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-03-31 14:26:02 +0100

    Remove commented out code

--- src/levels/level_editor/level_editor.gd
+++ src/levels/level_editor/level_editor.gd
@@ -144,10 +144,6 @@ func _build_level(level_data: LevelData) -> void:
 		var tiles: PoolVector2Array = level_data.tiles[type]
 		for pos in tiles:
 			set_cellv(pos, type)
-#			if Tiles.level_editor_to_main.has(type):
-#				set_cellv(pos, Tiles.level_editor_to_main[type])
-#			else:
-#				push_warning("Level editor tile %s has no corresponding main tile" % type)
 
 
 func _clear_level(value: bool) -> void:
